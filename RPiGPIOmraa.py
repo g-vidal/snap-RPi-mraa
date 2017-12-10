aa7ad9069dc30edcb3eb5f6e7131936f81f1aad5
g-vidal <gerard.vidal@ens-lyon.fr>
2017-12-11 00:35:11 +0100

    operational with sudo user

--- RPiGPIOmraa.py
+++ RPiGPIOmraa.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
 """
-Snap! extension to support Raspberry Pi -- server component.
-Copyright (C) 2014  Paul C. Brown <p_brown@gmx.com>.
+Snap!BYOB extension to interact with Raspberry Pi GPIO
+through mrra and upm libs.
+Python serversocket.
+Author G. Vidal <gerard.vidal@ens-lyon.fr>
+
+Snap!BYOB blocks are provided by RPiGPIOmraa.xml
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,83 +22,51 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import SimpleHTTPServer
-import mraa
-import os
+import socketserver
+import mraa, time
 import re
-import SocketServer
-import urllib2
 
-class CORSHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-	def send_head(self):
-		path = self.path
+class SnRPiHTTPRqHandler(socketserver.BaseRequestHandler):
+	
+	def handle(self):
+		# self.request is the TCP socket connected to the client
+		self.data = self.request.recv(2048).strip()
+		strData = str(self.data)
+		# just send back 'OK'
+		if strData.endswith("keep-alive'"):
+			self.request.sendall(str.encode('OK'))
+		print (self.data)
+		reObj = re.compile(".*pin=([0-9]*).*state=(LOW|HIGH|INF)")
+		reMatch = reObj.match(str(self.data))
+		self.pin = int(reMatch.group(1))
+		print ('Pin activated : {}'.format(self.pin))
+		led = mraa.Gpio(self.pin)
+		led.dir(mraa.DIR_OUT)
+		led.write(1)
+		if reMatch.group(2) == 'LOW':
+			led.write(0)
+		elif reMatch.group(2) == 'HIGH':
+			led.write(1)
+			time.sleep(10)
+		else :
+			led.dir(mraa.DIR_IN)
+
+	
+def main():
+	try:
+		HOST, PORT = '', 8185
+		Handler = SnRPiHTTPRqHandler
 		
-		# path looks like this:
-		# /pinwrite?pin=1&state=LOW
-		# or
-		# /pinread?pin=1&state=LOW
-		
-		self.pin=0
-		self.state=False
-		
-#		GPIO.setmode(GPIO.BCM)
-		
-		ospath = os.path.abspath('')
-		
-		regex = re.compile(".*pin=([0-9]*).*state=(LOW|HIGH)")
-		m = regex.match(path)
-		
-		if 'pinwrite' in path: # write HIGH or LOW to pin
+		with socketserver.TCPServer((HOST, PORT), Handler)  as server:
 			
-			self.pin = int(m.group(1))
-			self.state = True
-			if m.group(2) == 'LOW':
-				self.state = False
-				
-#			GPIO.setup(self.pin, GPIO.OUT)
-#			GPIO.output(self.pin, self.state)
-			led = mraa.Gpio(self.pin)
-			led.dir(mraa.DIR_OUT)
-			led.write(self.state)
-		
-		elif 'pinread'in path: # Read state of pin.
-			 """
-			 This function contains code borrowed heavily from 
-			 Technoboy10 (https://github.com/technoboy10). Look
-			 him up. His stuff is amazing!
-			 """
-			 
-			 self.pin = int(m.group(1))
-			 self.state = True
-			 if m.group(2) == 'LOW':
-				 self.state = False
-			 f = open(ospath + '/return', 'w+')
-			 GPIO.setup(self.pin, GPIO.IN)
-			 if(GPIO.input(self.pin) == self.state):
-				 f.write(str(True))
-			 else:
-				 f.write(str(False))
-			 f.close()
-			 f = open(ospath + '/return', 'rb')
-			 ctype = self.guess_type(ospath + '/rpireturn')
-			 self.send_response(200)
-			 self.send_header("Content-type", ctype)
-			 fs = os.fstat(f.fileno())
-			 self.send_header("Content-Length", str(fs[6]))
-			 self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
-			 self.send_header("Access-Control-Allow-Origin", "*")
-			 self.end_headers()
-			 return f
+			print ('serving at port : {}'.format(PORT))
+			print ('Use custom Rpi blocs in Snap!')
+			server.serve_forever()
+	except KeyboardInterrupt:
+		print ('Stop Request received, shutting down server')
+		server.socket.close()
 
- 
-if __name__ == "__main__":
-	PORT = 8280 #R+P in ASCII Decimal
-	Handler = CORSHTTPRequestHandler
-
-	httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-	print "serving at port", PORT
-	print "Go ahead and launch Snap!"
-	httpd.serve_forever()
+if __name__ == '__main__':
+    main()
 
 
